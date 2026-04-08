97a96e66a7e252b8571505b8f2211820fe4f97aa
Shaan Desai <leafjolt@gmail.com>
2026-04-08 09:41:52 +0200

    Replaced hardcoded localhost addresses with env variables

--- src/stores/gameStore.js
+++ src/stores/gameStore.js
@@ -52,7 +52,7 @@ export const useGameStore = defineStore('game', {
             }
         },
         async startGame() {
-            const response = await fetch('http://localhost:3000/api/questions/random')
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/questions/random`)
             const questions = await response.json()
             this.questions = questions
             this.currentIndex = 0
@@ -101,7 +101,7 @@ export const useGameStore = defineStore('game', {
             this.scoreSubmitted = false
         },
         async submitScore() {
-            const response = await fetch('http://localhost:3000/api/scores', {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/scores`, {
                 method: 'POST',
                 headers: {
                 'Content-Type': 'application/json',
@@ -117,7 +117,7 @@ export const useGameStore = defineStore('game', {
             }
         },
         async register(email, password) {
-            const response = await fetch('http://localhost:3000/api/auth/register', {
+            const response = await fetch('${import.meta.env.VITE_API_URL}/api/auth/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password })
@@ -127,7 +127,7 @@ export const useGameStore = defineStore('game', {
             return data
         },
         async login(email, password) {
-            const response = await fetch('http://localhost:3000/api/auth/login', {
+            const response = await fetch('${import.meta.env.VITE_API_URL}/api/auth/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password })
